3beb4ecc6b15f5ab2ccabe4feac9bbe6276dbfe3
Mark Steffensen <crazyeight68@gmail.com>
2018-07-30 01:12:23 -0700

    Worked on some of the framework for taking data from the user and sanitizing it.

--- ChangeReturnProgram/Program.cs
+++ ChangeReturnProgram/Program.cs
@@ -10,9 +10,31 @@ namespace ChangeReturnProgram
     {
         static void Main(string[] args)
         {
+            // Variable declaration
+            string userInput;
             int userMoney;
+            bool isNumeric = false;
 
-            Console.WriteLine("Please enter the cost of an item and ");
+            Console.WriteLine("Please enter the cost of an item and correct amount of change will be displayed!");
+
+            // Do-While ensures that the code is re-run until there is a valid number
+            do
+            {
+                
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out userMoney))
+                {
+                    Console.WriteLine("You chose " + userMoney);
+                }
+                else if (userMoney == 0)
+                {
+                    Console.WriteLine("Please enter a number!");
+                    isNumeric = false;
+                }
+
+            } while (isNumeric == false);
+
         }
     }
 }
