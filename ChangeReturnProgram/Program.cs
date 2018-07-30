ba49dad816510c860760fa222e67739eea8972c4
Mark Steffensen <crazyeight68@gmail.com>
2018-07-29 22:59:41 -0700

    Adding base project files

---
+++ ChangeReturnProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeReturnProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userMoney;
+
+            Console.WriteLine("Please enter the cost of an item and ");
+        }
+    }
+}
