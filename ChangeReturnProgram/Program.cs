1270dac0063afd252ed5098682608179abe93ce2
Mark Steffensen <crazyeight68@gmail.com>
2018-07-30 23:43:12 -0700

    Minor update with some more work done on basic scaffolding

--- ChangeReturnProgram/Program.cs
+++ ChangeReturnProgram/Program.cs
@@ -10,31 +10,51 @@ namespace ChangeReturnProgram
     {
         static void Main(string[] args)
         {
+
+
+
             // Variable declaration
             string userInput;
-            int userMoney;
+            //decimal itemPrice;
             bool isNumeric = false;
+            bool isRunning = true;
 
-            Console.WriteLine("Please enter the cost of an item and correct amount of change will be displayed!");
 
-            // Do-While ensures that the code is re-run until there is a valid number
+            Console.WriteLine("Welcome to the random price shop! Where you never really know what you are going to pay!");
+
+
+
             do
             {
-                
-                userInput = Console.ReadLine();
 
-                if (int.TryParse(userInput, out userMoney))
+                // Creates new random object
+                Random itemPrice = new Random(300);
+                Console.WriteLine(itemPrice);
+                //int itemPrice = itemPrice.Next(300);
+             
+
+
+                // Do-While ensures that the code is re-run until there is a valid number
+                do
                 {
-                    Console.WriteLine("You chose " + userMoney);
-                }
-                else if (userMoney == 0)
-                {
-                    Console.WriteLine("Please enter a number!");
-                    isNumeric = false;
-                }
 
-            } while (isNumeric == false);
+                    userInput = Console.ReadLine();
 
-        }
+                    // Both declaring and assigning 'userMoney' variable within the argument
+                    if (decimal.TryParse(userInput, out decimal userMoney))
+                    {
+                        userMoney = Decimal.Round(userMoney, 2, MidpointRounding.AwayFromZero);
+                        Console.WriteLine("You chose " + userMoney);
+                    }
+                    else if (userMoney == 0)
+                    {
+                        Console.WriteLine("Please enter a number!");
+                        isNumeric = false;
+                    }
+
+                } while (isNumeric == false);
+
+            } while (isRunning == true);
+            }
     }
 }
